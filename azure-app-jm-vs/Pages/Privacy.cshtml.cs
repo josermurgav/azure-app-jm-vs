9ff6650c583af314ba79f5ae4f2d01277eb709fd
Jose Murga <josermurgav@gmail.com>
2026-04-23 13:43:42 +0200

    Add project files.

---
+++ azure-app-jm-vs/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_app_jm_vs.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
